87c1e218272d78029eb09ea444a1de92c2ecb43c
JasonJWilliamsNY <JasonJWilliamsNY@gmail.com>
2018-08-01 15:45:03 -0400

    update comments on createusers.sh

--- jupyter-persistant/createusers.sh
+++ jupyter-persistant/createusers.sh
@@ -5,11 +5,19 @@
 # and for the case where the hub needs to be resarted.
 
 
-# Create users if needed
+# Create users if needed and deal with existing or missing persistant folders
+# and home directory symbolic links inside the container
+
+# Check to see if the user in the usernames.txt exists
 for user in $(cat /jupyter-persistant/usernames.txt)
   do
+  # Senario where user does not exist on the container (i.e launching or
+  # restarting a container )
   if ! id "$user" >/dev/null 2>&1
   then
+      # if the user does not exist in the container but they have a folder at
+      # jupyter-persistant/$user create the user and use their existing folder
+      # in jupyter-persistant/ as a target for a symbolic link to ~/$user
       if [ "/jupyter-persistant/$user" ]
       then
           echo "Using existing persistant folder for $user"
@@ -20,6 +28,10 @@ for user in $(cat /jupyter-persistant/usernames.txt)
           chown -R $user /jupyter-persistant/$user
           chown -R $user /home/$user
           echo "user $user added successfully!"
+      # if the user does not exist in the container and they do not have a
+      # folder in jupyter-persistant/ create jupyter-persistant/$user
+      # create the user on the container and use jupyter-persistant/$user as a
+      # target for a symbolic link to ~/$user
       else
           echo "Creating persistant folder for $user"
           cp -r /jupyter-persistant/skel /jupyter-persistant/$user
@@ -31,7 +43,12 @@ for user in $(cat /jupyter-persistant/usernames.txt)
           chown -R $user /home/$user
           echo "user $user added successfully!"
       fi
+  # Senario where user already exists in the container (i.e. restarting hub but
+  # not the container )
   else
+      # Check to see if the symbolic link ~/$user exists and that its target
+      # /jupyter-persistant exists. Create or use existing links and folders
+      # as needed
       if [ ! -L "/home/$user" ]
       then
           if [ ! -d "/jupyter-persistant/$user" ]
@@ -63,9 +80,11 @@ for user in $(cat /jupyter-persistant/usernames.txt)
   fi
   done
 
-# adjust which shell is used so that jupyterhub uses bash
+# adjust which shell is used so that jupyterhub uses bash in the meanest way
+# by deleting sh and using /bin/bash
 rm /bin/sh
 ln -s /bin/bash /bin/sh
+# todo: figure out how to get the Jupyter terminal to load bash profile 
 echo "SHELL=/bin/bash" >> /etc/environment
 
 jupyterhub --ip 0.0.0.0 --port 8000 -f /jupyter-persistant/jupyterhub_config.py
